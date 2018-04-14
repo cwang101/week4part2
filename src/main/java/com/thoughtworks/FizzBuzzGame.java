773e8fdd5cbc8d718cbd122aaed47d78e8383de2
cwang101 <625250454@qq.com>
2018-04-14 14:45:56 +0800

    complete!

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,18 +1,46 @@
 package com.thoughtworks;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
-    public void start(int count) {
+    private Map<Integer,String> map=new HashMap<>();
 
+    public FizzBuzzGame() {
+        map.put(3,"Fizz");
+        map.put(5,"Buzz");
+        map.put(7,"Whizz");
+    }
+
+    public void start(int count) {
+        for(int i=1;i<=count;i++){
+            results.add(getReport(i));
+        }
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String getReport(int num){
+        if(String.valueOf(num).contains("3")){
+            return "Fizz";
+        }
+        StringBuilder sb=new StringBuilder();
+        for(Map.Entry<Integer,String> entry:map.entrySet()){
+            if(num%entry.getKey()==0){
+                sb.append(entry.getValue());
+            }
+        }
+        if(sb.toString().length()<=0){
+            return String.valueOf(num);
+        }
+        return sb.toString();
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,76 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="Fizz";
+        String result=game.getReport(13);
+        assertThat(result, equalTo(expectedResult));
+    }
 
+    @Test
+    public void should_return_Fizz_when_divisible_by_3() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="Fizz";
+        String result=game.getReport(6);
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @Test
+    public void should_return_Buzz_when_divisible_by_5() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="Buzz";
+        String result=game.getReport(10);
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @Test
+    public void should_return_Whizz_when_divisible_by_7() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="Whizz";
+        String result=game.getReport(14);
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_divisible_by_3_and_5() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="FizzBuzz";
+        String result=game.getReport(15);
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_divisible_by_3_and_7() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="FizzWhizz";
+        String result=game.getReport(21);
+        assertThat(result, equalTo(expectedResult));
+    }
+    @Test
+    public void should_return_BuzzWhizz_when_divisible_by_5_and_7() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="BuzzWhizz";
+        String result=game.getReport(70);
+        assertThat(result, equalTo(expectedResult));
+    }
+    @Test
+    public void should_return_FizzBuzzWhizz_when_divisible_by_3_and_5_and_7() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="FizzBuzzWhizz";
+        String result=game.getReport(105);
+        assertThat(result, equalTo(expectedResult));
+    }
+    @Test
+    public void should_return_origin_when_not_divisible_by_any() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        String expectedResult="4";
+        String result=game.getReport(4);
+        assertThat(result, equalTo(expectedResult));
     }
 }
